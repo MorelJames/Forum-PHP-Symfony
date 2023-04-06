1ea8bb68c2d67f63234bbdecfda4b3bd5cd53cda
morel james <james.morel@etu.univ-lyon1.fr>
2023-04-06 18:40:58 +0200

    création de la vue Category/AllCategory.html.twig et début de mise en forme

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,7 @@
 import './scss/app.scss';
 import './scss/header.scss';
+import './scss/category.scss';
 import './styles/app.css';
 
 
 import './bootstrap';
-import $ from 'jquery';
-import 'popper.js';
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,16 +3,36 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
-class CreateCategoryController extends AbstractController
+class CategoryController extends AbstractController
 {
 
-    #[Route('/createCategory')]
+    #[Route('/Category', name: 'app_category')]
+    public function Category(ManagerRegistry $doctrine, EntityManagerInterface $entityManager)
+    {
+
+        $categories = $doctrine->getRepository(Category::class)->findAll();
+
+        $date = new \Datetime();
+
+        $posts = $entityManager
+                 ->getRepository(Post::class)->getThreeLastPosts();
+
+        return $this->render('Category/AllCategory.html.twig', [
+            'categories' => $categories,
+            'newPosts' => $posts
+        ]);
+    }
+    
+
+    #[Route('/createCategory', name: 'app_createCategory')]
     public function createCategory(ManagerRegistry $doctrine, Request $request)
     {
 
